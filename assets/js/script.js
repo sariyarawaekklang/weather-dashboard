05f39b3aa172517175b97d0b6013f55456bdab88
sariyarawaekklang <sariyarawaekklang@gmail.com>
2022-01-15 19:25:18 -0600

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,8 @@ var formSubmitHandler = function(event) {
 };
 
 var getWeather = function(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+    // API key: 3091f3d1a43e5d3ae359427ebb960344
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=3091f3d1a43e5d3ae359427ebb960344";
 
     fetch(apiUrl)
         .then(function(response) {
@@ -29,6 +30,5 @@ var getWeather = function(city) {
 };
 
 var displayWeather = function(weather, searchedCity) {
-    var today = new Date();
-    document.getElementById()
+
 }
